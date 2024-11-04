86ba5a3eb3ff5a19a140200197965f95f72ca7ad
Rocky islam <mohammadrockyislam01@gmail.com>
2024-11-04 17:52:00 +0600

    header done

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,43 @@
+import image1 from '../../assets/image1.png'
+import vector from '../../assets/Vector.png'
+
+const Home = () => {
+    return (
+        <div>
+        <section className='mb-14'>
+            <div className="bg-[linear-gradient(123.4deg,_#BD1F17_40.8%,_#A61D13_76.4%,_#8E1B0F_120.69%)] w-full h-[1080px] flex items-center ">
+                <div className='flex justify-center items-center'>
+                    <div className="flex flex-col  items-center justify-center ">
+                        <div className='z-10 absolute left-80'>
+                        <h1 className="text-[80px] text-white font-bold uppercase backdrop-blur-sm bg-red-500/30 p-1">Taste the authentic <br />
+                        Saudi cuisine</h1>
+                        <p className='text-white'>Among the best Saudi chefs in the world, serving <br /> you something beyond flavor. </p>
+                        <div>
+                        <button className="btn btn-warning mt-9">EXPLORE MENU</button>
+                        </div>
+                        </div>
+                    </div>
+                    <div className='absolute right-[150px]'>
+                        <div className='relative'>
+                            <div >
+                                <img src={image1} alt="" className='rounded-xl'/>
+                                <div className='absolute -top-5 -right-5'>
+                                    <img src={vector} alt="" />
+                                </div>
+                                <div className='rounded-full border-2 h-20 w-20 flex items-center justify-center bg-warning font-medium absolute -right-10 -bottom-3'>
+                                    <p className='uppercase'>Today <br /> offer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+
+        </section>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/component/Nav/Nav.jsx
@@ -0,0 +1,68 @@
+import { NavLink } from "react-router-dom";
+
+import logo from '../../assets/Logo.png'
+import { IoIosMenu } from "react-icons/io";
+
+const Nav = () => {
+  const navLink = (
+    <>
+      <NavLink className="mx-2 my-2">
+        <li>Home</li>
+      </NavLink>
+      <NavLink className="mx-2 my-2">
+        <li>About</li>
+      </NavLink>
+      <NavLink className="mx-2 my-2">
+        <li>Portfolio</li>
+      </NavLink>
+      <NavLink className="mx-2 my-2">
+        <li>Clients</li>
+      </NavLink>
+      <NavLink className="mx-2 my-2">
+        <li>Blog</li>
+      </NavLink>
+      <NavLink className="mx-2 my-2">
+        <li>Contact</li>
+      </NavLink>
+    </>
+  );
+
+  return (
+    <div className="bg-[linear-gradient(123.4deg,_#BD1F17_40.8%,_#A61D13_76.4%,_#8E1B0F_120.69%)] w-full sticky top-0 z-50">
+      <div className="navbar bg-transparent lg:justify-around justify-between">
+        <div className=""> 
+
+            {/*
+             background: linear-gradient(123.4deg, #BD1F17 40.8%, #A61D13 76.4%, #8E1B0F 120.69%); 
+            bg-gradient-to-r from-[#BD1F17] from-40% via-[#A61D13] via-60% to-[#8E1B0F] to-[100%]
+
+            */}
+          <img src={logo} alt="logo" className="md:w-[200px] w-36"/>
+          <div className=" hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-white">
+            {navLink}
+          </ul>
+        </div>
+        </div>
+        
+        <div className=""> {/* nav flex*/}
+          <a className="btn hidden lg:flex bg-[#FEBF00]">BOOK A TABLE</a>
+{/* dropdown menu */}
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <IoIosMenu size={25}/>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content rounded-box z-[1] mt-3 p-2 shadow bg-[linear-gradient(123.4deg,_#BD1F17_40.8%,_#A61D13_76.4%,_#8E1B0F_120.69%)] "
+            >
+              {navLink}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
